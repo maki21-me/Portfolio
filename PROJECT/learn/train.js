ffba62de52aecfd1e148516ff658c1de0c29c7a7
Meklit Anteneh <meklitanteneh58@gmail.com>
2025-02-25 23:32:36 -0800

    update design

---
+++ PROJECT/learn/train.js
@@ -0,0 +1,20 @@
+/*let js='javascript is funny';
+if(js==='javascript is funny')
+console.log('javascript is fun');
+
+let j='amazing';
+console.log(10+10);
+
+console.log('jonas');
+console.log(23);
+*/
+let firstName='jonas';
+let job='software developer';
+const jonasNew ='i am ${firstName}, a $ ${job}!';
+console.log(jonasNew);
+
+const age=19;
+const isOldEnough=age>=18;
+if (isOldEnough){
+    console.log('Sarah can start driving licence ');
+}
